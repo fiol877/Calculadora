9424e98d4b778b56bcc48692a05dd899e509d794
fiol877 <fiol877@DESKTOP-FOKNO27>
2018-09-26 17:05:14 +0200

    Afexit reconeixement de divisio impossible

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -10,14 +10,23 @@ public class Calculadora {
         System.out.print("Diguem un valor: ");
         int A = in.nextInt();        
         System.out.print("Diguem un altre valor: ");
-        int B = in.nextInt(2);
-    int s= A+B; 
+        int B = in.nextInt();
+        
+    int s=A+B; 
     int r=A-B;
     int m=A*B;
-    int d=A/B;
+
         System.out.println(s);
         System.out.println(r);
         System.out.println(m);
-        System.out.println(d);
+    
+   if (A==0 || B==0){
+   System.out.println("Divisio impossible");
+   } else {
+       int d=A/B;
+       System.out.println(d);     
     }
+    
+        
+   }
 }
