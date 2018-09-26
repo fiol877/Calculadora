dcfeddb0103fe601b7c6d6d2bc882af6c137f96b
fiol877 <fiol877@DESKTOP-FOKNO27>
2018-09-26 16:20:16 +0200

    Afexit entrada de dos nombres enters i escaner

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,9 +1,16 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
     public static void main(String[] args) {
-
+    Scanner in = new Scanner(System.in);
+        System.out.print("Diguem un valor: ");
+        int A = in.nextInt();        
+        System.out.print("Diguem un altre valor: ");
+        int B = in.nextInt();
+        
     }
     
 }
